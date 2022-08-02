16bf0afa9dc4e325d180b7cca996a5f40a68d586
Simon Wang <jellalleonhardt4869@gmail.com>
2022-08-02 18:06:47 +0800

    feat: complete examples demo

--- examples/demo.go
+++ examples/demo.go
@@ -1,5 +1,18 @@
 package examples
 
-// marshal
+import (
+	"github.com/cloudwego/fastpb"
+)
 
-// unmarshal
+// Marshal
+func Marshal(msg fastpb.Writer) []byte {
+	buf := make([]byte, msg.Size())
+	msg.FastWrite(buf)
+	return buf
+}
+
+// Unmarshal
+func Unmarshal(buf []byte, msg fastpb.Reader) error {
+	_, err := fastpb.ReadMessage(buf, int8(fastpb.SkipTypeCheck), msg)
+	return err
+}
